6fe5a43606f309db4f720db338e4e66d516ef5ea
Sardonyx001 <lassouedjamel@hotmail.fr>
2023-07-31 18:16:35 +0900

    Removed unneeded dependencies

--- src/main/java/com/example/demo/SharedStateService.java
+++ src/main/java/com/example/demo/SharedStateService.java
@@ -7,8 +7,6 @@ import org.springframework.stereotype.Service;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Setter
 @Getter
